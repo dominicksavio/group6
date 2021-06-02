662cd4cd89cd8b52f6b5b5c7c52a4eb68955f0cf
Dominic <dominic@cloud-computer.c.computer-314802.internal>
2021-06-02 11:19:07 +0000

    adding location hash

--- dbscan/src/server.py
+++ dbscan/src/server.py
@@ -7,67 +7,78 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import normalize
 from sklearn.decomposition import PCA
 from flask import Flask
+from location_connect import *
 def cluster():
+	try:
+		connection = psycopg2.connect(user="postgres",
+		                                  password="postgres",
+		                                  host="172.17.0.2",
+		                                  port="5432",
+		                                  database="roadmakerDB")
+		cursor = connection.cursor()
+		sql_select_query = """SELECT latitude,longitude,hash FROM hash_location Where latitude!='not found'"""
+		cursor.execute(sql_select_query)
+		record = cursor.fetchall()
 
-	connection = psycopg2.connect(user="postgres",
-	                                  password="postgres",
-	                                  host="172.17.0.2",
-	                                  port="5432",
-	                                  database="roadmakerDB")
-	cursor = connection.cursor()
-	sql_select_query = """SELECT latitude,longitude,hash FROM hash_location Where latitude!='not found'"""
-	cursor.execute(sql_select_query)
-	record = cursor.fetchall()
 
+		X=record
 
-	X=record
+		X_normalized=X
 
-	X_normalized=X
+		# Converting the numpy array into a pandas DataFrame
+		X_normalized = pd.DataFrame(X_normalized)
+		X_normalized = X_normalized.drop(2, axis = 1)
+		pca = PCA(n_components = 2)
+		X_principal = X_normalized
+		X_principal = pd.DataFrame(X_principal)
+		X_principal.columns = ['Latitude', 'Longitude']
+		# print(X_principal.head())
+		# Numpy array of all the cluster labels assigned to each data point
 
-	# Converting the numpy array into a pandas DataFrame
-	X_normalized = pd.DataFrame(X_normalized)
-	X_normalized = X_normalized.drop(2, axis = 1)
-	pca = PCA(n_components = 2)
-	X_principal = X_normalized
-	X_principal = pd.DataFrame(X_principal)
-	X_principal.columns = ['Latitude', 'Longitude']
-	# print(X_principal.head())
-	# Numpy array of all the cluster labels assigned to each data point
+		db_default = DBSCAN(eps = 0.0375, min_samples = 2).fit(X_principal)
+		labels = db_default.labels_
+		# print(labels)
+		out={}
+		img=[]
+		for i in range(len(labels)):
+			if(labels[i] in out):
+				out[labels[i]] +=1
+			else:
+				out[labels[i]]=1
+				img.append(record[i])
+			# out.append([labels[i],X[i]])
+		# print(record)
+		# print(out)
 
-	db_default = DBSCAN(eps = 0.0375, min_samples = 2).fit(X_principal)
-	labels = db_default.labels_
-	# print(labels)
-	out={}
-	img=[]
-	for i in range(len(labels)):
-		if(labels[i] in out):
-			out[labels[i]] +=1
-		else:
-			out[labels[i]]=1
-			img.append(record[i])
-		# out.append([labels[i],X[i]])
-	# print(record)
-	# print(out)
-
-	for k in out:
-		# print(k,out[k])
-		# print(img[k][2])
-		# print(str(k), str(out[k]),str(img[k][2]))
-		try:
-			sql_update_query = """insert into public.accounts_clusters(cluster,frequency,image_hash,status,updated_image,lat,lon) values(%s,%s,%s,'NA','NA',%s,%s)"""
-			cursor.execute(sql_update_query, (str(k), str(out[k]),str(img[k][2]),str(img[k][0]),str(img[k][1])))
-			connection.commit()
-		except (Exception, psycopg2.Error) as error:
-			print("Error in update operation", error)
+		for k in out:
+			# print(k,out[k])
+			# print(img[k][2])
+			# print(str(k), str(out[k]),str(img[k][2]))
+			try:
+				sql_update_query = """insert into public.accounts_clusters(cluster,frequency,image_hash,status,updated_image,lat,lon) values(%s,%s,%s,'NA','NA',%s,%s)"""
+				cursor.execute(sql_update_query, (str(k), str(out[k]),str(img[k][2]),str(img[k][0]),str(img[k][1])))
+				connection.commit()
+				return "Clustered"
+			except (Exception, psycopg2.Error) as error:
+				print("Error in update operation", error)
+				return "not Clustered"
+	except (Exception, psycopg2.Error) as error:
+			print("Error in select operation", error)
+			return "not Clustered"
 server = Flask(__name__)
-
 @server.route("/")
+@server.route("/cluster")
 def hello():
-	cluster()
-	return "Clustered"
+	out=cluster()
+	return out
+@server.route("/location")
+def location():
+	out=updateTable()
+	return out
 
 if __name__ == "__main__":
 	server.run(host='0.0.0.0')
+
 
 # cluster_afterVerify()
 #######################################################################################
